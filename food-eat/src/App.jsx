8cca5c4cf8cb7aa0fa801e5ec249b9d31708813c
jagnyadatta <jagnyadattadalai99215@gmail.com>
2024-10-25 22:44:32 +0530

    added all files

---
+++ food-eat/src/App.jsx
@@ -0,0 +1,15 @@
+import Navbar from "./components/Navbar/Navbar"
+import Hero from "./components/Hero/Hero";
+
+function App() {
+  return (
+    <>
+        <div className="overflow-x-hidden">
+          <Navbar />
+          <Hero />
+        </div>
+    </>
+  )
+}
+
+export default App
---
+++ food-eat/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import Profile from "../../assets/profile.png"
+import { IoIosArrowDown } from "react-icons/io";
+
+const Navbar = () => {
+  return (
+    <div className='py-4'>
+      <div className="container flex justify-between items-center">
+        {/* logo section  */}
+        <div>
+          <p className='text-lg font-semibold'>
+            Food<span className='text-red-500'>Plaza</span>
+          </p>
+        </div>
+        {/* menu section  */}
+        <div className='flex justify-center items-center gap-6'>
+          <ul className='gap-8 hidden sm:flex'>
+            <li className='hover:border-b-4 border-primary cursor-pointer'>Home</li>
+            <li className='hover:border-b-4 border-primary cursor-pointer'>Menu</li>
+            <li className='hover:border-b-4 border-primary cursor-pointer'>About</li>
+          </ul>
+          {/* Login section */}
+          <div className='flex items-center'>
+            <img src={Profile} alt="" className='w-12 rounded-full'/>
+            <IoIosArrowDown/>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
